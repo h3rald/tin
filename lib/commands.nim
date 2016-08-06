c426aa7a903a91648e8d323aad6516c10dc0d9f6
h3rald <h3rald@h3rald.com>
2016-08-06 23:18:29 +0200

    Documented all commands developed so far.

--- lib/commands.nim
+++ lib/commands.nim
@@ -117,7 +117,10 @@ COMMANDS["prepare"] = proc(ctx: var TinContext): int =
     newPackage(dir = getCurrentDir(), name = ctx.args[1], version = v)
     success "Tin package \"$1\" (version: $2) initialized." % [ctx.args[1], v] 
 
-# open <tin> <folder>
+cmd("open")
+  .desc("Uncompresses a tin package into a specific folder.")
+  .arg("tin").desc("The tin package file to open.").cmd
+  .arg("folder").desc("The folder where the package contents will be placed.")
 COMMANDS["open"] = proc(ctx: var TinContext): int =
   if ctx.args.len < 3:
     error "Package file or destination not specified"
@@ -126,14 +129,16 @@ COMMANDS["open"] = proc(ctx: var TinContext): int =
     ctx.args[1].uncompress(ctx.args[2])
     success "Tin package '$1' opened successfully in '$1'." % [ctx.args[1], ctx.args[2]]
 
-# fill
+cmd("fill")
+  .desc("Creates a .tin.zip package file in the current directory containing a valid tin.json file.")
 COMMANDS["fill"] = proc(ctx: var TinContext): int =
   execute(30):
     let pkg = loadPackage()
     pkg.compress(getCurrentDir())
     success "Package $1 v$2 ready." % [pkg.name, pkg.version]
 
-# store <tin>
+cmd("store")
+  .arg("tin").desc("Stores the specified .tin.zip package into the local storage directory.")
 COMMANDS["store"] = proc(ctx: var TinContext): int =
   if ctx.args.len < 2:
     error "Package file not specified"
@@ -145,7 +150,9 @@ COMMANDS["store"] = proc(ctx: var TinContext): int =
     let pkgdata = ctx.storage.store(ctx.args[1])
     success "Package $1 v$2 stored." % [pkgdata.name, pkgdata.version]
 
-# label major|minor|patch
+cmd("label")
+  .desc("Increments the major, minor, or patch version of the current package.")
+  .arg("label").desc("The version digit to incremenr (major|minor|patch).")
 COMMANDS["label"] = proc(ctx: var TinContext): int =
   if ctx.args.len < 2:
     error "Label not specified"
@@ -167,7 +174,9 @@ COMMANDS["label"] = proc(ctx: var TinContext): int =
     pkg.setVersion(version)
     success "Package version set to $1." % $version
 
-# relabel <version>
+cmd("relabel")
+  .desc("Resets the version of the current package.")
+  .arg("version").desc("A valid (semantic) version number (e.g. 1.2.0).")
 COMMANDS["relabel"] = proc(ctx: var TinContext): int =
   if ctx.args.len < 2:
     error "Label not specified"
@@ -181,7 +190,8 @@ COMMANDS["relabel"] = proc(ctx: var TinContext): int =
     pkg.setVersion(label.newVersion())
     success "Package version set to $1." % $label
 
-# inventory
+cmd("inventory")
+  .desc("Scans the local storage folder and re-builds the package list saved in the .tinrc file.")
 COMMANDS["inventory"] = proc(ctx: var TinContext): int =
   execute(70):
     ctx.storage.scan()
@@ -191,13 +201,15 @@ COMMANDS["inventory"] = proc(ctx: var TinContext): int =
       for r in pkg.releases:
         echo "   - $1" % r
 
-# scrap <tin> [<version>]
+cmd("scrap")
+  .desc("Deletes all the releases of the specified package.")
+  .opt("version").desc("If specified, deletes only the specific version of the package.")
 COMMANDS["scrap"] = proc(ctx: var TinContext): int =
   if ctx.args.len < 2:
     error "Package not specified"
     return 80
   let name = ctx.args[1]
-  if ctx.args.len < 3:
+  if not ctx.opts.hasKey("version"):
     # Delete al releases of package
     execute(81):
       if not ctx.storage.hasPackage(name):
@@ -206,7 +218,7 @@ COMMANDS["scrap"] = proc(ctx: var TinContext): int =
       success "Package '$1' deleted." % name
   else:
     # Delete a specific version of package
-    let version = ctx.args[2]
+    let version = ctx.opts["version"]
     if not version.validVersion:
       error "Invalid version: $1" % version
       return 82
